dfcf49b0ee2ac3c5996ea949331ffbc68fc9913d
luyao <luyaohappy@gmail.com>
2018-09-27 10:38:51 +0800

    a lot interesting alias for shell commands

---
+++ zsh/aliases.zsh
@@ -0,0 +1,37 @@
+# reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+# IP addresses
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i enp0s3 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+# remove broken symlinks
+alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
+
+# File size
+alias fs="stat -f \"%z bytes\""
