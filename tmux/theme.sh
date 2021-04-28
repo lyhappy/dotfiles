aa8b46d77c2a0217efef090c438faccb24df54bc
luyao <luyaohappy@gmail.com>
2021-04-28 17:35:23 +0800

    update tmux config

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -1,10 +1,22 @@
 #### COLOUR
 
-tm_color_active=colour32
-tm_color_inactive=colour241
-tm_color_feature=colour206
-tm_color_music=colour215
-tm_active_border_color=colour240
+
+base00=default   # - Default
+base01='#151515' # - Lighter Background (Used for status bars)
+base02='#202020' # - Selection Background
+base03='#909090' # - Comments, Invisibles, Line Highlighting
+base04='#505050' # - Dark Foreground (Used for status bars)
+base05='#D0D0D0' # - Default Foreground, Caret, Delimiters, Operators
+base06='#E0E0E0' # - Light Foreground (Not often used)
+base07='#F5F5F5' # - Light Background (Not often used)
+base08='#AC4142' # - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+base09='#D28445' # - Integers, Boolean, Constants, XML Attributes, Markup Link Url
+base0A='#F4BF75' # - Classes, Markup Bold, Search Text Background
+base0B='#90A959' # - Strings, Inherited Class, Markup Code, Diff Inserted
+base0C='#75B5AA' # - Support, Regular Expressions, Escape Characters, Markup Quotes
+base0D='#6A9FB5' # - Functions, Methods, Attribute IDs, Headings
+base0E='#AA759F' # - Keywords, Storage, Selector, Markup Italic, Diff Changed
+base0F='#8F5536' # - Deprecated, Opening/Closing Embedded Language Tags, e.g. <? php ?>
 
 # separators
 tm_separator_left_bold="◀"
@@ -17,43 +29,38 @@ set -g status-right-length 150
 set -g status-interval 5
 
 # default statusbar colors
-# set-option -g status-bg colour0
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-# set-option -g status-attr default
+set-option -g status-fg $base0D
+set-option -g status-bg colour0
 
 # default window title colors
-# set-window-option -g window-status-fg $tm_color_inactive
-# set-window-option -g window-status-bg default
+set-window-option -g window-status-style fg=$base03,bg=default
 set -g window-status-format "#I #W"
 
 # active window title colors
-# set-window-option -g window-status-current-fg $tm_color_active
-# set-window-option -g window-status-current-bg default
-# set-window-option -g  window-status-current-format "#[bold]#I #W"
+set-window-option -g window-status-current-style fg=$base0D,bg=default
+set-window-option -g  window-status-current-format "#[bold]#I #W"
 
 # pane border
-# set-option -g pane-border-fg $tm_color_inactive
-# set-option -g pane-active-border-fg $tm_active_border_color
+set-option -g pane-border-style fg=$base03
+set-option -g pane-active-border-style fg=$base0B
 
 # message text
-# set-option -g message-bg default
-# set-option -g message-fg $tm_color_active
+set-option -g message-style bg=default,fg=$base0A
 
 # pane number display
-set-option -g display-panes-active-colour $tm_color_active
-set-option -g display-panes-colour $tm_color_inactive
+set-option -g display-panes-active-colour $base0F
+set-option -g display-panes-colour $base03
 
 # clock
-set-window-option -g clock-mode-colour $tm_color_active
+set-window-option -g clock-mode-colour $base0D
 
 # tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/tunes.scpt | cut -c 1-50)"
-tm_tunes="#[fg=$tm_color_music]#(osascript -l JavaScript ~/.dotfiles/applescripts/tunes.js)"
-tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
+# tm_tunes="#[fg=$base04]#(osascript -l JavaScript ~/.dotfiles/applescripts/tunes.js)"
+# tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
 
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
-tm_host="#[fg=$tm_color_feature,bold]#h"
-tm_session_name="#[fg=$tm_color_feature,bold]#S"
+tm_date="#[fg=$base03] %R %d %b"
+tm_host="#[fg=$base0E,bold]#h"
+tm_session_name="#[fg=$base0E,bold]#S"
 
 set -g status-left $tm_session_name' '
 set -g status-right $tm_tunes' '$tm_date' '$tm_host
