dca451fe6e4855d63893c7fe974c212dc6cdd319
luyao <luyaohappy@gmail.com>
2025-04-07 19:41:57 +0800

    update nvim config, using lua scripts

---
+++ config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,80 @@
+-----------------------------------------------------------
+-- Define keymaps of Neovim and installed plugins.
+-----------------------------------------------------------
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap=true, silent=true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Change leader to a comma
+vim.g.mapleader = ','
+
+-----------------------------------------------------------
+-- Neovim shortcuts
+-----------------------------------------------------------
+
+-- Disable arrow keys
+map('', '<up>', '<nop>')
+map('', '<down>', '<nop>')
+map('', '<left>', '<nop>')
+map('', '<right>', '<nop>')
+
+-- Map Esc to kk
+map('i', 'jk', '<Esc>')
+
+-- map('n', ';', ':')
+
+-- Clear search highlighting with <leader> and c
+map('n', '<leader>c', ':nohl<CR>')
+
+-- Toggle auto-indenting for code paste
+-- map('n', '<F2>', ':set invpaste paste?<CR>')
+-- vim.opt.pastetoggle = '<F2>'
+
+-- Change split orientation
+map('n', '<leader>tk', '<C-w>t<C-w>K') -- change vertical to horizontal
+map('n', '<leader>th', '<C-w>t<C-w>H') -- change horizontal to vertical
+
+-- Move around splits using Ctrl + {h,j,k,l}
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
+-- Reload configuration without restart nvim
+map('n', '<leader>r', ':so %<CR>')
+
+-- Fast saving with <leader> and s
+map('n', '<leader>w', ':w<CR>')
+
+-- Close all windows and exit from Neovim with <leader> and q
+map('n', '<leader>q', ':q<CR>')
+
+-----------------------------------------------------------
+-- Applications and Plugins shortcuts
+-----------------------------------------------------------
+
+-- Terminal mappings
+map('n', '<C-t>', ':Term<CR>', { noremap = true })  -- open
+map('t', '<Esc>', '<C-d><C-n>')                    -- exit
+
+-- NvimTree
+map('n', '<C-n>', ':NvimTreeToggle<CR>')            -- open/close
+map('n', '<leader>fr', ':NvimTreeRefresh<CR>')       -- refresh
+map('n', '<leader>ff', ':NvimTreeFindFile<CR>')      -- search file
+
+-- Tagbar
+map('n', '<leader>z', ':TagbarToggle<CR>')          -- open/close
+
+-- Git
+-- for plugin tpope/vim-fugitive
+map('n', '<leader>gd', ':Gvdiffsplit<cr>')
+map('n', '<leader>gw', ':Gwriter<cr>')
+map('n', '<leader>gr', ':Gread<cr>')
+map('n', '<leader>gc', ':Git commit<cr>')
+map('n', '<leader>gl', ':Gclog<cr>')
+map('n', '<leader>gb', ':Git blame<cr>')
