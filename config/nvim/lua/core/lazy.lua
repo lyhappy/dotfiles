d0918777829e95d4223c93776ecfe354a826bfd2
luyao <luyaohappy@gmail.com>
2025-10-04 22:27:01 +0800

    add neovim plugin: which-key, render-markdown

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -127,5 +127,32 @@ lazy.setup({
 
     { "itchyny/calendar.vim" },
     -- { 'rcarriga/nvim-notify' }
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {},
+    },
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    }
   }
 })
