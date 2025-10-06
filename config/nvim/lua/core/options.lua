33fd9b60aa46a7108ccde0a77cf1c7b7d919277f
luyao <luyaohappy@gmail.com>
2025-10-07 00:01:46 +0800

    nvim: set tabstop as 2

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -35,8 +35,8 @@ opt.scrolloff = 7
 -- Tabs, indent
 -----------------------------------------------------------
 opt.expandtab = true        -- Use spaces instead of tabs
-opt.shiftwidth = 4          -- Shift 4 spaces when tab
-opt.tabstop = 4             -- 1 tab == 4 spaces
+opt.shiftwidth = 2          -- Shift 4 spaces when tab
+opt.tabstop = 2             -- 1 tab == 4 spaces
 opt.smartindent = true      -- Autoindent new lines
 
 -----------------------------------------------------------
