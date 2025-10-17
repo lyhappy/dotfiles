aa561e797e1d1e0df71f83eae00aa8716bc6a65f
luyao <luyaohappy@gmail.com>
2025-10-17 10:00:57 +0800

    nvim config: ga for git add within nvim-tree

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -11,11 +11,80 @@ local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
   return
 end
+local api = require "nvim-tree.api"
 
 -- Call setup.
 -- See: `:help nvim-tree` 4. SETUP
 -- Each of these are documented in `:help nvim-tree.OPTION_NAME`
 -- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`)
+local function edit_or_open()
+  local node = api.tree.get_node_under_cursor()
+
+  if node.nodes ~= nil then
+    -- expand or collapse folder
+    api.node.open.edit()
+  else
+    -- open file
+    api.node.open.edit()
+    -- Close the tree if file was opened
+    api.tree.close()
+  end
+end
+
+-- open as vsplit on current node
+local function vsplit_preview()
+  local node = api.tree.get_node_under_cursor()
+
+  if node.nodes ~= nil then
+    -- expand or collapse folder
+    api.node.open.edit()
+  else
+    -- open file as vsplit
+    api.node.open.vertical()
+  end
+
+  -- Finally refocus on tree if it was lost
+  api.tree.focus()
+end
+
+local git_add = function()
+  local node = api.tree.get_node_under_cursor()
+  local gs = node.git_status.file
+
+  -- If the current node is a directory get children status
+  if gs == nil then
+    gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
+         or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+  end
+
+  -- If the file is untracked, unstaged or partially staged, we stage it
+  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+    vim.cmd("silent !git add " .. node.absolute_path)
+
+  -- If the file is staged, we unstage
+  elseif gs == "M " or gs == "A " then
+    vim.cmd("silent !git restore --staged " .. node.absolute_path)
+  end
+
+  api.tree.reload()
+end
+
+local function my_on_attach(bufnr)
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+
+  vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
+  vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
+  vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+  vim.keymap.set("n", "ga", git_add,              opts("Git Add"))
+end
+
 nvim_tree.setup {
   auto_reload_on_write = true,
   --disable_netrw = false, -> already disabled on `/core/options.lua`
@@ -28,7 +97,7 @@ nvim_tree.setup {
   sync_root_with_cwd = false,
   reload_on_bufenter = false,
   respect_buf_cwd = false,
-  on_attach = "disable",
+  on_attach = my_on_attach,
   select_prompts = false,
   view = {
     centralize_selection = false,
@@ -162,7 +231,7 @@ nvim_tree.setup {
     show_on_open_dirs = true,
     timeout = 400,
   },
-    modified = {
+  modified = {
     enable = false,
     show_on_dirs = true,
     show_on_open_dirs = true,
