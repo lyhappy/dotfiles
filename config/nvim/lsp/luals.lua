b8503eea6239b7c5758f4411826b31dd89a9fef8
luyao <luyaohappy@gmail.com>
2025-10-17 10:03:05 +0800

    nvim config: suppress undefined global 'vim'

--- config/nvim/lsp/luals.lua
+++ config/nvim/lsp/luals.lua
@@ -6,13 +6,16 @@ return {
   -- Sets the "workspace" to the directory where any of these files is found.
   -- Files that share a root directory will reuse the LSP server connection.
   -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
   -- Specific settings to send to the server. The schema is server-defined.
   -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
   settings = {
     Lua = {
       runtime = {
         version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { "vim" }
       }
     }
   }
